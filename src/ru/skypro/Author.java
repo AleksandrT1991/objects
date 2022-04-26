43d7b884846d96284ee541e60d159546916d8963
AleksandrTsygulev <aleksandr.tsygulev@gmail.com>
2022-04-26 23:09:25 +0300

    на проверку

---
+++ src/ru/skypro/Author.java
@@ -0,0 +1,20 @@
+package ru.skypro;
+
+public class Author {
+    public String authorFirstName;
+    public String authorSecondName;
+
+    public Author(String authorFirstName, String authorSecondName) {
+        this.authorFirstName = authorFirstName;
+        this.authorSecondName = authorSecondName;
+    }
+    public String getAuthorName(){
+        return authorFirstName + " " + authorSecondName;
+    }
+    public String getAuthorFirstName(){
+        return authorFirstName;
+    }
+    public String getAuthorSecondName(){
+        return authorSecondName;
+    }
+}
